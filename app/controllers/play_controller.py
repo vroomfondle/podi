5fb635c30c540b60ca02a9fea21e049f357ea84f
Peter Frost <slimeypete@gmail.com>
2014-11-22 22:07:43 +0000

    use movieid instead of file path when playing movies

--- app/controllers/play_controller.py
+++ app/controllers/play_controller.py
@@ -1,7 +1,7 @@
 from cement.core import controller
 from lib.podi.rpc.library.movies import list_movies
 from lib.podi.rpc.library.tv_shows import list_tv_shows
-from lib.podi.rpc.player import play_file
+from lib.podi.rpc.player import play_file, play_movie, play_tv_show
 
 class PlayController(controller.CementBaseController):
   class Meta:
@@ -17,27 +17,27 @@ class PlayController(controller.CementBaseController):
 
   @controller.expose()
   def movie(self):
-    id = self.app.pargs.positional_arguments[0]
+    movie_id = self.app.pargs.positional_arguments[0]
     try:
       movie = [movie_details for movie_details in self.app.send_rpc_request(list_movies())['result']['movies'] 
-        if str(movie_details['movieid']) == id][0]
+        if str(movie_details['movieid']) == movie_id][0]
     except IndexError, e:
-      self.app.log.error("Movie {0} not found.".format(id))
+      self.app.log.error("Movie {0} not found.".format(movie_id))
       return False
     # Movie exists if no AttributeError was thrown; try to play it
-    self.app.log.info("Playing movie {0}: {1} ({2})".format(id, movie['label'], movie['file']))
-    self.app.send_rpc_request(play_file(movie['file']))
+    self.app.log.info("Playing movie {0}: {1} ({2})".format(movie_id, movie['label'], movie['file']))
+    self.app.send_rpc_request(play_movie(movie_id))
 
 
   @controller.expose(aliases=['tv', 'tvshow', 'show'])
   def tv_show(self):
-    id = self.app.pargs.positional_arguments[0]
+    tv_show_id = self.app.pargs.positional_arguments[0]
     try:
       tvshow = [tvshow_details for tvshow_details in self.app.send_rpc_request(list_tv_shows())['result']['tvshows'] 
-        if str(tvshow_details['tvshowid']) == id][0]
+        if str(tvshow_details['tvshowid']) == tv_show_id][0]
     except IndexError, e:
       self.app.log.error("Movie {0} not found.".format(id))
       return False
     # Movie exists if no AttributeError was thrown; try to play it
     self.app.log.info("Playing tvshow {0}: {1} ({2})".format(id, tvshow['label'], tvshow['file']))
-    self.app.send_rpc_request(play_file(tvshow['file']))
+    self.app.send_rpc_request(play_tv_show(tv_show_id))
--- lib/podi/rpc/player/player.py
+++ lib/podi/rpc/player/player.py
@@ -7,5 +7,25 @@ def play_file(file_path):
       "params": {
             "item": {"file": file_path}
       },
-      "id": "play"
+      "id": "play_file"
+  }
+
+def play_movie(movie_id):
+  return {
+      "jsonrpc": "2.0",
+      "method": "Player.Open",
+      "params": {
+            "item": {"movieid": int(movie_id)}
+      },
+      "id": "play_movie_{0}".format(movie_id)
+  }
+
+def play_tv_show(tv_show_id):
+  return {
+      "jsonrpc": "2.0",
+      "method": "Player.Open",
+      "params": {
+            "item": {"tvshowid": int(tv_show_id)}
+      },
+      "id": "play_tv_show_{0}".format(tv_show_id)
   }
