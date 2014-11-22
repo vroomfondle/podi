41e7e8db6f607a9da4caf61b65e14e523fea9d3f
Peter Frost <slimeypete@gmail.com>
2014-11-22 21:17:23 +0000

    added introspection to the rpc model

---
+++ lib/podi/rpc/introspect.py
@@ -0,0 +1,16 @@
+def introspect_method(method_name):
+  """Can be used to retrieve documentation for the given JSON RPC method"""
+  if method_name == "" or method_name is None:
+    raise ValueError("Method name must not be empty")
+
+  return {
+      "jsonrpc": "2.0",
+      "method": "JSONRPC.Introspect",
+      "params": {
+          "filter": {
+              "id": method_name,
+              "type": "method"
+          }
+      },
+      "id": "introspect_{0}".format(method_name)
+  }
