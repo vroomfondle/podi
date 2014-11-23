99ba24c1469d92ad7b4531b5df5662f507518ca1
Peter Frost <slimeypete@gmail.com>
2014-11-23 00:38:14 +0000

    make executable and add more sensible default text

--- podi.py
+++ podi.py
@@ -5,13 +5,16 @@ A simple utility to list, play, pause and stop media files in XBMC/Kodi.
 
 
 from app import PodiApplication
-from app.controllers import ListController, PlayController
+from app.controllers import ListController, PlayController, IntrospectController, PauseController, StopController
 from cement.core import handler
 
 
 app = PodiApplication()
 handler.register(ListController)
 handler.register(PlayController)
+handler.register(PauseController)
+handler.register(StopController)
+handler.register(IntrospectController)
 
 try:
   app.setup()
