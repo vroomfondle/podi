f3daa280f86119324e9856c3c68de1673b71d3fc
Peter Frost <slimeypete@gmail.com>
2015-04-30 19:32:17 +0100

    slightly improve the unit tests so that they actually test some stuff

--- app/tests/podi_tests.py
+++ app/tests/podi_tests.py
@@ -15,11 +15,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-#!/usr/bin/env python3
 
 """
 A few simple tests for Podi
 """
+from unittest.mock import patch
+from nose.tools import raises
 from cement.utils import test
 from app.podi_application import PodiApplication
 from app.controllers import ListController, PlayController,\
@@ -27,43 +28,157 @@ from app.controllers import ListController, PlayController,\
     CleanupController, UpdateController, InspectController,\
     ResumeController
 from cement.core import handler
-
-
-class FakePodi(PodiApplication):
-
-    class Meta:
-        argv = []
+from app.errors import NoMediaError
 
 
 class TestPodi(test.CementTestCase):
-    app_class = FakePodi
+    app_class = PodiApplication
 
-    def test_podi_default(self):
+    def setUp(self):
+        self.app = PodiApplication(argv=[])
+
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_default(self, mock_sender):
         """Check that the app bootstraps without errors"""
         self.app.setup()
         self.app.run()
         self.app.close()
 
-    def test_podi_list_shows(self):
-        """Check that the 'list shows' function runs without errors"""
-        self.app = FakePodi(argv=["list", "shows"])
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_list_shows(self, mock_sender):
+        """
+        Check that the 'list shows' function runs without errors
+        and makes the correct RPC call. 
+        """
+        self.app = PodiApplication(argv=["list", "shows"])
         handler.register(ListController)
         self.app.setup()
         self.app.run()
         self.app.close()
 
-    def test_podi_list_movies(self):
-        """Check that the 'list movies' function runs without errors"""
-        self.app = FakePodi(argv=["list", "movies"])
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # check that the correct RPC method was called at least once
+        shows_rpc_sent = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'VideoLibrary.GetTVShows':
+                shows_rpc_sent = True
+        assert shows_rpc_sent
+
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_list_movies(self, mock_sender):
+        """
+        Check that the 'list movies' function runs without errors
+        and makes the correct RPC call. 
+        """
+        self.app = PodiApplication(argv=["list", "movies"])
         handler.register(ListController)
         self.app.setup()
         self.app.run()
         self.app.close()
 
-    def test_podi_list_episodes(self):
-        """Check that the 'list episodes' function runs without errors"""
-        self.app = FakePodi(argv=["list", "episodes", "1"])
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # check that the correct RPC method was called at least once
+        movies_rpc_sent = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'VideoLibrary.GetMovies':
+                movies_rpc_sent = True
+        assert movies_rpc_sent
+
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_list_episodes(self, mock_sender):
+        """
+        Check that the 'list episodes' function runs without errors as long as
+        at least one episode exists for the given show, and makes the correct
+        RPC call.
+        """
+        mock_sender.return_value = {'episodes': [{'title':'test','runtime': 10,'episodeid':1}]}
+
+        self.app = PodiApplication(argv=["list", "episodes", "1"])
         handler.register(ListController)
         self.app.setup()
         self.app.run()
         self.app.close()
+
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # check that the correct RPC method was called at least once
+        episodes_rpc_sent = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'VideoLibrary.GetEpisodes':
+                episodes_rpc_sent = True
+        assert episodes_rpc_sent
+
+    @raises(NoMediaError)
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_list_episodes_none(self, mock_sender):
+        """
+        Check that the 'list episodes' function throws an error
+        if no episodes exist for the given show
+        """
+        mock_sender.return_value = {}
+
+        self.app = PodiApplication(argv=["list", "episodes", "1"])
+        handler.register(ListController)
+        self.app.setup()
+        self.app.run()
+        self.app.close()
+
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # check that the correct RPC method was called at least once
+        episodes_rpc_sent = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'VideoLibrary.GetEpisodes':
+                episodes_rpc_sent = True
+        assert episodes_rpc_sent
+
+
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_play_episode(self, mock_sender):
+        self.app = PodiApplication(argv=["play", "episode", "1"])
+        handler.register(PlayController)
+        self.app.setup()
+        self.app.run()
+        self.app.close()
+
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # the most important RPC call is the one to Player.Open, so make sure that's been called
+        # with the correct id
+        player_opened = False
+        correct_movie_id = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'Player.Open':
+                player_opened = True
+                assert call_args[0][0]['params']['item']['episodeid'] == 1
+        assert player_opened
+
+        
+    @patch('app.podi_application.PodiApplication.send_rpc_request')
+    def test_podi_play_movie(self, mock_sender):
+        self.app = PodiApplication(argv=["play", "movie", "1"])
+        handler.register(PlayController)
+        self.app.setup()
+        self.app.run()
+        self.app.close()
+
+        # at least one RPC call should have been made
+        assert len(mock_sender.call_args) >= 1
+
+        # the most important RPC call is the one to Player.Open, so make sure that's been called
+        # with the correct id
+        player_opened = False
+        correct_movie_id = False
+        for call_args in mock_sender.call_args_list:
+            if call_args[0][0]['method'] == 'Player.Open':
+                player_opened = True
+                assert call_args[0][0]['params']['item']['movieid'] == 1
+        assert player_opened
+
